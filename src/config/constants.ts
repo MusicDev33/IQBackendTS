08449c54491cee5d9ca581afbb281a4ed421c2eb
MusicDev33 <smccowan33@gmail.com>
2019-12-22 01:54:31 -0700

    API name fixes

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,14 +2,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 let port = 3002;
-let apiBase = '/api/v2/';
+let apiBase = '/tsapi/v1/';
 
 if (process.env.NODE_ENV === 'DEVTEST') {
   port = 3003;
 }
 
 if (process.env.NODE_ENV === 'DEVTEST' || process.env.NODE_ENV === 'production') {
-  apiBase = '/v2/';
+  apiBase = '/v1/';
 }
 
 const acceptedAgents = ['IQAPIv1', 'IQiOSv1', 'IQAndroidv1'];
