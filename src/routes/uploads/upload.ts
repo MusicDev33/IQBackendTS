38ac167d229c144880e869d36d25b812f7e2a006
MusicDev33 <smccowan33@gmail.com>
2020-02-19 12:12:48 -0700

    Test

--- src/routes/uploads/upload.ts
+++ src/routes/uploads/upload.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 
 export const uploadQuestionRoute = async (req: Request, res: Response) => {
+  /*
   if (req.file) {
     console.log(req.file);
     const file = req.file as any;
@@ -9,5 +10,10 @@ export const uploadQuestionRoute = async (req: Request, res: Response) => {
     return res.status(200).json({msg: 'File uploaded successfully!', fileURL: fileURL});
   } else {
     return res.status(500).json({msg: 'Something went wrong...'});
-  }
+  }*/
+  console.log(req.file);
+  const file = req.file as any;
+  const fileURL = 'https://cdn.inquantir.com/' + file['key'];
+  console.log('File uploaded successfully.');
+  return res.status(200).json({msg: 'File uploaded successfully!', fileURL: fileURL});
 };
